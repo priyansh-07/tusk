5fcdb36b83793e3adc3ffba86acc8ff852ce31bd
Priyansh <priyansh7299@gmail.com>
2021-12-25 14:42:13 +0530

    added rendering logic based on AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignInSide from "./components/SignIn";
+import { DashBoard } from "./components/DashBoard";
+import { AuthContext } from "./contexts/AuthContext";
+import React, { useContext } from "react";
 
 function App() {
+  const context = useContext(AuthContext);
+
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={context?.auth.isLoggedIn ? <DashBoard /> : <SignInSide />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/DashBoard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DashBoard: React.FC<React.ReactNode> = props => {
+  return(
+    <h1>Dash</h1>
+  )
+}
